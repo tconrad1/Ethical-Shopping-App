a0a721027fbaa6a2fb7c17fa207374bbe1dea082
tconrad1 <tconrad1@umd.edu>
2024-01-12 18:23:57 -0500

    very rough draft of search

--- ethical shopping/src/App.js
+++ ethical shopping/src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
+const companyArray = [ ['nestle', 'evil'], ['other', 'ok']
+];
+
+export function MainInput() {
+   const [Company, setCompany] = useState('');
+   var out = '';
+
+   let i = 0;
+   while (i<companyArray.length){
+    if(companyArray[i][0] == Company){
+     out = Company + " " + companyArray[i][1];
+    }
+    i+=1;
+   }
+
+
+  return (
+    <>
+    <label>
+    Company/Product:
+    <input 
+    value= {Company}
+    onChange= {e=> setCompany(e.target.value)}
+    
+    
+    />
+    
+    </label>
+    <br></br>
+    {out != '' && <a>
+      Ethical Concerns:  {out} </a>}
+
+     
+    </>
+
+
+
+
+  );
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div >
+      
+    {MainInput("")}
     </div>
+    
   );
 }
 
