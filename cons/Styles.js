4b5139ec8ebdea5bcd6e2f0aa1f316f76e9137a0
razzberrylemon <kelse.taylor.250@gmail.com>
2024-01-14 19:42:29 -0500

    adding stylesheet

---
+++ cons/Styles.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+
+export const textS = StyleSheet.create ({
+   label: {
+    fontFamily: 'Georgia',
+    color: '#00072D',
+    fontSize: 20,
+    padding: 10
+    
+   },
+
+   smallText: {
+      fontFamily: 'Georgia',
+      fontSize: 17,
+      padding: 10
+
+   },
+
+   tempText: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 20
+   },
+
+   buttonStyle: {
+      backgroundColor: "#00072D",
+      borderRadius: 6,
+      alignItems: 'center',
+      padding: 5,
+      height: 30,
+      width: 200
+   },
+
+   buttonText: {
+      fontFamily: 'Georgia',
+      color: '#FAF5F3',
+      
+   },
+})
