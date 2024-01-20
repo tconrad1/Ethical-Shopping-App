a075195f634c1531498bb6c033999e8ff72f2015
arthakar <149721532+arthakar@users.noreply.github.com>
2024-01-19 21:08:06 -0500

    Add files via upload
    Script for Rolo, let me know if I did it correctly.

---
+++ cons/search_components/search-results/Companies/Nestle-Products/Rolo.js
@@ -0,0 +1,44 @@
+// Rolo.js
+import React from 'react';
+import { View, TouchableOpacity, Text, Image } from 'react-native';
+
+import { Styles } from '../../Styles';
+
+const Rolo = ({ navigation }) => {
+    const handleProductPress = () => {
+      navigation.navigate('NestleInfoScreen', { searchEntry: 'Nestle' });
+    };
+
+  return (
+    <TouchableOpacity onPress={handleProductPress}>
+      <View style={Styles.productAssembler}>
+        <View style={Styles.productContainer}>
+          <Image
+            style={Styles.productImage}
+            source={require('./Product-Images/Rolo.png')}
+          />
+          <View style={Styles.productTextContainer}>
+            <Text style={Styles.productTextHeaders}>
+                Company:
+            </Text>
+            <Text style={Styles.productTextText}>
+                Nestle
+            </Text>
+            <Text style={Styles.productTextHeaders}>
+                Rating:
+            </Text>
+            <Text style={Styles.productUnethical}>
+                Unethical
+            </Text>
+            <Text style={Styles.productTextText}>
+              Tap to see more information 
+              {"\n"}and alternatives
+            </Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default Rolo;
