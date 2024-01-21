c3aae8dce460d81b7578209d935af2478569a032
bgybanez <bybanez@terpmail.umd.edu>
2024-01-21 16:43:30 -0500

    ignore

--- cons/search_components/search-results/SearchScreen.js
+++ cons/search_components/search-results/SearchScreen.js
@@ -6,10 +6,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SearchBar from '../BeasSearchBar.js';
 import SearchResultsScreen from './SearchResultsScreen.js';
-
 import NestleInfoScreen from './Companies/Nestle-Products/NestleInfoScreen.js';
 import TraderJoesInfoScreen from './Companies/TraderJoes/TraderJoesInfoScreen.js';
-
 import News from '../News.js';
 import { Styles } from './Styles';
 
@@ -18,32 +16,29 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer independent={true}>
+    <NavigationContainer independent = {true}>
       <Stack.Navigator>
-        <Stack.Screen name="SearchScreen" options={{headerShown:false}} >
+        <Stack.Screen name = "SearchScreen" options = {{headerShown:false}}>
           {() => (
-            <SafeAreaView style={{ flex: 1 }}>
+            <SafeAreaView style = {{flex: 1}}>
               <ScrollView>
-                <View style={Styles.searchBackground}>
-                  <View style={Styles.searchTopBar}>
-                    <View style={Styles.searchOtherBar}>
-                      <SearchBar />
+                <View style = {Styles.searchBackground}>
+                  <View style = {Styles.searchTopBar}>
+                    <View style = {Styles.searchOtherBar}>
+                      <SearchBar/>
                     </View>
                   </View>
-                  <News />
+                  <News/>
                 </View>
               </ScrollView>
             </SafeAreaView>
           )}
         </Stack.Screen>
         <Stack.Screen 
-          name="SearchResultsScreen" 
-          component={SearchResultsScreen}
-          options={({navigation}) => ({navigation}, {headerShown:false})
-          }
-        />
-        <Stack.Screen name="NestleInfoScreen" component={NestleInfoScreen} />
-        <Stack.Screen name="TraderJoesInfoScreen" component={TraderJoesInfoScreen} />
+          name = "SearchResultsScreen" component = {SearchResultsScreen}
+          options = {({navigation}) => ({navigation}, {headerShown:false})}/>
+        <Stack.Screen name = "NestleInfoScreen" component = {NestleInfoScreen}/>
+        <Stack.Screen name = "TraderJoesInfoScreen" component = {TraderJoesInfoScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
